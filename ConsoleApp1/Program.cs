cff21885a11560e42f9936d0cd5a2347a103b56a
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2025-01-14 21:10:45 +0000

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class Taxi 
+{
+    public string brandName;
+
+    public Taxi() 
+    {
+        brandName = "BMW";
+    }
+}
+
+class TestTaxi 
+{
+    static void Main () 
+    {
+        Taxi t = new Taxi();
+        Console.WriteLine(t.brandName);
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,21 @@
+﻿// This is my fist C# code
+// Hello Student
+
+using System; //system is a namespace
+
+class FirstProgram 
+{
+    static void Main (string[] args) // entry point of the code
+    {
+
+        int num1, num2, result;
+        num1 = 100;
+        num2 = 200;
+        result = num1 + num2;
+
+        Console.WriteLine("Number1 = "  +num1);
+        Console.WriteLine("Number2 = "  +num2);
+        Console.WriteLine("Addition = " +result);
+
+    }
+}
